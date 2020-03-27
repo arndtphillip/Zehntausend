158b738c2725cd4cd5e0f2e02b9030815423838a
arndtphillip <arndt.philipp@gmx.de>
2020-03-27 17:51:36 +0100

    Implemented header and footer for scire table

--- app/src/main/java/com/parndt/zehntausend/activities/GameActivity.java
+++ app/src/main/java/com/parndt/zehntausend/activities/GameActivity.java
@@ -2,17 +2,17 @@ package com.parndt.zehntausend.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.parndt.zehntausend.R;
 import com.parndt.zehntausend.adapters.PlayerScoreAdapter;
+import com.parndt.zehntausend.adapters.PlayerScoreFooterAdapter;
+import com.parndt.zehntausend.adapters.PlayerScoreHeaderAdapter;
 import com.parndt.zehntausend.model.GameState;
 import com.parndt.zehntausend.model.Player;
 import com.parndt.zehntausend.model.PlayerScore;
@@ -23,13 +23,16 @@ import java.util.List;
 public class GameActivity extends AppCompatActivity {
 
     private GameState state;
-    private RecyclerView.Adapter adapter;
+    private RecyclerView.Adapter scoreAdapter;
+    private RecyclerView.Adapter footerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.tableScores);
+        RecyclerView headerRecyclerView = (RecyclerView) findViewById(R.id.scoreHeader);
+        RecyclerView scoreRecyclerView = (RecyclerView) findViewById(R.id.tableScores);
+        RecyclerView footerRecyclerView = (RecyclerView) findViewById(R.id.scoreFooter);
 
         Bundle extras = getIntent().getExtras();
         ArrayList<Parcelable> parcelables = extras.getParcelableArrayList(NewGameActivity.EXTRA_PLAYERS);
@@ -45,18 +48,27 @@ public class GameActivity extends AppCompatActivity {
 
         state = new GameState(players);
 
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, players.size());
-        recyclerView.setLayoutManager(layoutManager);
+        headerRecyclerView.setLayoutManager(new GridLayoutManager(this, players.size()));
+        scoreRecyclerView.setLayoutManager(new GridLayoutManager(this, players.size()));
+        footerRecyclerView.setLayoutManager(new GridLayoutManager(this, players.size()));
 
-        adapter = new PlayerScoreAdapter(players);
-        recyclerView.setAdapter(adapter);
+        PlayerScoreHeaderAdapter headerAdapter = new PlayerScoreHeaderAdapter(players);
+        headerRecyclerView.setAdapter(headerAdapter);
+
+        scoreAdapter = new PlayerScoreAdapter(players);
+        scoreRecyclerView.setAdapter(scoreAdapter);
+
+        footerAdapter = new PlayerScoreFooterAdapter(players);
+        footerRecyclerView.setAdapter(footerAdapter);
     }
 
     /** adds new points to the player scores */
     public void addScore(View view) {
         EditText pointsText = (EditText) findViewById(R.id.pointsText);
         state.addScore(Integer.parseInt(pointsText.getText().toString()));
-        adapter.notifyDataSetChanged();
+
+        scoreAdapter.notifyDataSetChanged();
+        footerAdapter.notifyDataSetChanged();
 
         pointsText.getText().clear();
     }
--- app/src/main/java/com/parndt/zehntausend/adapters/PlayerScoreAdapter.java
+++ app/src/main/java/com/parndt/zehntausend/adapters/PlayerScoreAdapter.java
@@ -3,17 +3,13 @@ package com.parndt.zehntausend.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.parndt.zehntausend.R;
 import com.parndt.zehntausend.model.PlayerScore;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
