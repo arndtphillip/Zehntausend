1c316c3a75a9166e5a7db5f288b1b1a3581583dc
arndtphillip <arndt.philipp@gmx.de>
2020-06-09 19:12:25 +0200

    Added intent animations

--- app/src/main/java/com/parndt/zehntausend/activities/MainActivity.java
+++ app/src/main/java/com/parndt/zehntausend/activities/MainActivity.java
@@ -1,11 +1,13 @@
 package com.parndt.zehntausend.activities;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 
 import com.parndt.zehntausend.R;
 import com.parndt.zehntausend.Zehntausend;
@@ -30,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         GameState resumeState = Zehntausend.gameState;
 
         Button resumeButton = (Button) findViewById(R.id.resumeGameButton);
-        if (!resumeState.isStarted()) {
+        if (!resumeState.isStarted() || resumeState.gameOver()) {
             resumeButton.setVisibility(View.GONE);
         } else {
             resumeButton.setVisibility(View.VISIBLE);
--- app/src/main/java/com/parndt/zehntausend/activities/NewGameActivity.java
+++ app/src/main/java/com/parndt/zehntausend/activities/NewGameActivity.java
@@ -2,8 +2,11 @@ package com.parndt.zehntausend.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
